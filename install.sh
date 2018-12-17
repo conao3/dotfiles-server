96a9b50a01099b68ee3d1060da8abce5591cd453
conao3 <conao3@gmail.com>
2018-12-17 13:36:41 +0900

    enable shallow clone

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 # Conao3 dotfiles hosting server.<br />
 # See <a href=https://github.com/conao3/dotfiles_server>GitHub</a> to get more information.<br />
 # <!--
-git clone https://github.com/conao/dotfiles.git ~/.dotfiles
+git clone --depth 1 https://github.com/conao/dotfiles.git ~/.dotfiles
 make -C ~/.dotfiles
 # -->
