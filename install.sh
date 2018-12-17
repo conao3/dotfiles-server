ced00b009cbf7f9c8aba013e934b666b2e54ce3b
conao3 <conao3@gmail.com>
2018-12-17 12:15:35 +0900

    fix link (use <a> html tag)

--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 # <br />
 # Conao3 dotfiles hosting server.<br />
-# See https://github.com/conao3/dotfiles_server to get more information.<br />
+# See <a href=https://github.com/conao3/dotfiles_server>GitHub</a> to get more information.<br />
 # <!--
 git clone https://github.com/conao/dotfiles.git ~/.dotfiles
 make -C ~/.dotfiles
 # -->
-
-
-
