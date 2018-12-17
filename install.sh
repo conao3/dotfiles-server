f657c6522e6a2b20a57dd52214146c839ddf9fdb
conao3 <conao3@gmail.com>
2018-12-17 11:47:45 +0900

    html, sh hybrid file

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-
+# Conao3 dotfiles hosting server.<br />
+# See https://github.com/conao3/dotfiles_server to get more information.<br />
+# <!--
 git clone https://github.com/conao/dotfiles.git ~/.dotfiles
 make -C ~/.dotfiles
+# -->
+
+
+
