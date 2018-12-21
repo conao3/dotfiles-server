56f330be2e8e5037adae06640a0f14d6efe93061
conao3 <conao3@gmail.com>
2018-12-21 20:56:38 +0900

    fix install comamnd

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@
 # See <a href=https://github.com/conao3/dotfiles_server>GitHub</a> to get more information.<br />
 # <!--
 git clone --depth 1 https://github.com/conao/dotfiles.git ~/.dotfiles
-make -C ~/.dotfiles
+cd ~/.dotfiles
+make
 # -->
